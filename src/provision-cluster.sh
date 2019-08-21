d994129861ccfaa02d7cceba37d921118064e52a
Alex Bulankou <alexbu@google.com>
2019-08-21 15:19:20 +0300

    Simplifying provisioning

--- src/provision-cluster.sh
+++ src/provision-cluster.sh
@@ -3,22 +3,22 @@
 PROJECT_ID=[PROJECT_ID]
 CLUSTER_ID=cluster-1
 ZONE=us-central1-a
-KCC_BUNDLE_DIR=kcc-install-bundle/0.0.9/install-bundle
+KCC_BUNDLE_DIR=kcc-install-bundle/0.0.10/install-bundle
 
 gcloud config set project $PROJECT_ID
 # Note: this creates a cluster with workload identity enabled, using Beta API
 gcloud beta container clusters create ${CLUSTER_ID} --identity-namespace=${PROJECT_ID}.svc.id.goog --zone $ZONE
 gcloud container clusters get-credentials $CLUSTER_ID --zone=$ZONE
 
-SA_EMAIL="cnrm-system@${PROJECT_ID}.iam.gserviceaccount.com"
-SA_EMAIL_DEMO="cnrm-application-demo@${PROJECT_ID}.iam.gserviceaccount.com"
 
-# for each cluster
+# install KCC
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
-kubectl apply -R -f ${KCC_BUNDLE_DIR}
+kubectl apply -f ${KCC_BUNDLE_DIR}
+
+# give cnrm-system namespace permissions to manage GCP
 kubectl create secret generic gcp-key --from-file ./key.json --namespace cnrm-system
 
-# for each namespace
-kubectl create secret generic gcp-key --from-file=./key-editor.json
-kubectl create secret generic gcp-key-owner --from-file ./key.json
+# for each namespace generic gcp-key-owner --from-file ./key.json
+
+
 kubectl annotate namespace default "cnrm.cloud.google.com/project-id=${PROJECT_ID}" --overwrite
--- src/provision-project.sh
+++ src/provision-project.sh
@@ -3,19 +3,14 @@
 PROJECT_ID=[PROJECT_ID]
 ZONE=us-central1-a
 
-gcloud config set project $PROJECT_ID
+ls $PROJECT_ID
 
 SA_EMAIL="cnrm-system@${PROJECT_ID}.iam.gserviceaccount.com"
-SA_EMAIL_DEMO="cnrm-application-demo@${PROJECT_ID}.iam.gserviceaccount.com"
 
 # once per project
 gcloud iam service-accounts create cnrm-system --project ${PROJECT_ID}
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SA_EMAIL}" --role roles/owner
 gcloud iam service-accounts keys create --iam-account "${SA_EMAIL}" ./key.json
-
-gcloud iam service-accounts create cnrm-application-demo --project ${PROJECT_ID}
-gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${SA_EMAIL_DEMO}" --role roles/editor
-gcloud iam service-accounts keys create --iam-account ${SA_EMAIL_DEMO} ./key-editor.json
 
 gcloud services enable pubsub.googleapis.com --project ${PROJECT_ID}
 gcloud services enable spanner.googleapis.com --project ${PROJECT_ID}
